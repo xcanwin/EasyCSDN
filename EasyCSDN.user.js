816e36b460e6590d68441085a18f2f1a955abe27
xcanwin <xcanwin@gmail.com>
2023-11-24 16:25:49 +0800

    发布

---
+++ EasyCSDN.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         EasyCSDN
+// @description  简洁、专注、高效的CSDN，清理99%多余的元素，只展示文章正文和关联文章
+// @version      1.0
+// @author       xcanwin
+// @namespace    https://github.com/xcanwin/EasyCSDN/
+// @supportURL   https://github.com/xcanwin/EasyCSDN/
+// @license      GPL-2.0-only
+// @match        *://blog.csdn.net/*/article/details/*
+// @match        *://bbs.csdn.net/topics/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+.passport-login-container, /*隐藏登录提示*/
+.passport-login-tip-container, /*隐藏登录权益提示*/
+#toolbarBox, /*隐藏顶部*/
+.blog_container_aside, /*隐藏左边栏*/
+#rightAside, /*隐藏右边栏*/
+.csdn-side-toolbar, /*隐藏右边栏2*/
+.blog-footer-bottom, /*隐藏底部*/
+.recommend-nps-box, /*隐藏打分*/
+.left-toolbox /*隐藏关注*/
+{
+    display: none !important;
+}
+
+/*隐藏背景*/
+body {
+    background: none;
+    background-color: #f5f6f7;
+}
+
+/*正文居中*/
+#mainBox {
+    display: flex;
+    justify-content: center;
+}
+
+/*展示全屏*/
+#mainBox, #mainBox main {
+    width: 100%;
+}
+.main_father.d-flex {
+    display: unset !important;
+}
+`);
+
+})();
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# EasyCSDN
-简洁、专注、高效的CSDN，清理99%多余的元素，只展示文章正文和关联文章
+## 项目简介
+
+- 喜欢这个插件的小伙伴，可以给我的GITHUB项目 [EasyCSDN](https://github.com/xcanwin/EasyCSDN) 点个⭐️STAR支持一下。
+- 简洁、专注、高效的CSDN，清理99%多余的元素，只展示文章正文和关联文章
+
+## 安装渠道
+
+官方渠道✅:
+
+| 序号 | UserScript源 | 备注 |
+| --- | --- | --- |
+| 1 | [Github开源社区](https://raw.githubusercontent.com/xcanwin/EasyCSDN/main/EasyCSDN.user.js) | ✅推荐 |
+| 2 | [GreasyFork开源社区](https://greasyfork.org/zh-CN/scripts/480668-easycsdn) | ✅推荐 |
