9dcd3425a16f88f790bb1eea7074b779e1ac947f
xcanwin <xcanwin@gmail.com>
2023-11-25 23:37:03 +0800

    新增支持移动端

--- EasyCSDN.user.js
+++ EasyCSDN.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EasyCSDN
 // @description  这是一款提高CSDN简洁度的插件。它可以让你的CSDN学习之路变得简洁、专注、高效、畅快。主要功能是净化页面，净化99%多余元素，只展示文章正文和关联文章。
-// @version      7.0
+// @version      8.0
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/EasyCSDN/
 // @supportURL   https://github.com/xcanwin/EasyCSDN/
@@ -17,15 +17,16 @@
 (function() {
     'use strict';
 
+    /*电脑端*/
     GM_addStyle(`
-.passport-login-container, /*隐藏登录提示*/
-.passport-login-tip-container, /*隐藏登录权益提示*/
-#toolbarBox, /*隐藏顶部*/
-.blog_container_aside, /*隐藏左边栏*/
-#rightAside, /*隐藏右边栏*/
-.csdn-side-toolbar, /*隐藏右边栏-磁吸*/
-.blog-footer-bottom, /*隐藏底部*/
-.recommend-nps-box, /*隐藏打分*/
+.passport-login-container /*隐藏登录提示*/,
+.passport-login-tip-container /*隐藏登录权益提示*/,
+#toolbarBox /*隐藏顶部*/,
+.blog_container_aside /*隐藏左边栏*/,
+#rightAside /*隐藏右边栏*/,
+.csdn-side-toolbar /*隐藏右边栏-磁吸*/,
+.blog-footer-bottom /*隐藏底部*/,
+.recommend-nps-box /*隐藏打分*/,
 .left-toolbox /*隐藏关注*/,
 .blog-tags-box /*隐藏开头的专栏收录*/,
 #blogColumnPayAdvert /*隐藏结尾的专栏收录*/,
@@ -63,6 +64,32 @@ body {
 #toolBarBox {
     margin-bottom: 4rem;
 }
+`);
+
+    /*移动端*/
+    GM_addStyle(`
+#csdn-toolbar /*隐藏顶部*/,
+#operate /*隐藏底部评论和搜索提示*/,
+.aside-header-fixed /*隐藏关注*/
+{
+    display: none !important;
+}
+
+/*展示全屏*/
+body #main {
+    padding-top: unset !important;
+    margin-top: unset !important;
+}
+body {
+    padding-bottom: unset !important;
+}
+
+/*展示分界线*/
+.spec_space {
+    background-color: #ffebeb !important;
+    height: 32px !important;
+}
+
 `);
 
 })();
